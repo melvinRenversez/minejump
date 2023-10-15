f0e106fd055aabdc233317cb40eb21993d0b8111
melvinRenversez <120313822+melvinRenversez@users.noreply.github.com>
2023-10-16 00:12:48 +0200

    main

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var body = document.body;
 var timeText = document.getElementById("time");
 var steve = document.getElementById("steve");
+var enemy = document.createElement("div");  
 
 var time = 0;
 var isJumping = false;
+var isAttacking = false;
+var speedAttack = 3;
+
+steve.style.border = "1px solid black";
+enemy.style.border = "1px solid black";
+
 
 document.addEventListener("keydown", function(event){
     if (event.key === " " && !isJumping){
@@ -18,11 +25,34 @@ document.addEventListener("keydown", function(event){
 })
 
 function enemyAttack(){
-    var enemy = document.createElement("div");
-    enemy.style.height = "170px";
-    enemy.style.width = "100px";
+    enemy.style.height = "165px";
+    enemy.style.width = "43px";
     enemy.style.background = "url(img/zombie.png) no-repeat";
+    enemy.style.position = "absolute";
+    enemy.style.left = "97%";
+    enemy.style.bottom = "21%";
     body.appendChild(enemy);
+    if (!isAttacking){
+        void steve.offsetWidth; 
+        isAttacking = true;
+        enemy.style.animation = "attack " + speedAttack + "s linear";
+        setTimeout(function(){
+            isAttacking = false;
+            enemy.remove();
+            enemyAttack();
+        }, speedAttack * 1000);
+    }
+}
+
+function Colliding(){
+    var steveRect = steve.getBoundingClientRect();
+    var enemyRect = enemy.getBoundingClientRect();
+    return(
+        steveRect.left < enemyRect.right &&
+        steveRect.right > enemyRect.left &&
+        steveRect.top < enemyRect.bottom &&
+        steveRect.bottom > enemyRect.top 
+    )
 }
 
 function updateTime(){
@@ -35,11 +65,15 @@ function updateTime(){
      
     setTimeout(function(){
         time += 1; 
-        console.log(time);
         updateTime();
     }, 1000);
 }
 
+setInterval(function () {
+    if (Colliding()) {
+        alert("Game Over");
+    }
+}, 16);
 
 updateTime();
 enemyAttack();
